a5ce834f6fb408ee77cf2c814b3fb88163cdec42
albert19_ <albertnanda1902@gmail.com>
2023-12-17 05:22:33 +0800

    seperate blog form and schme into different component

---
+++ app/dashboard/schema/index.tsx
@@ -0,0 +1,28 @@
+import * as z from "zod"
+
+export const BlogFormSchema = z.object({
+    title: z.string().min(2, {
+        message: "Title must be at least 2 characters.",
+    }),
+    image_url: z.string().url({ message: "Must be a valid image url." }),
+    content: z.string().min(2, {
+        message: "Content must be at least 2 characters.",
+    }),
+    is_published: z.boolean(),
+    is_premium: z.boolean(),
+}).refine((data) => {
+    const image_url = data.image_url;
+
+    try {
+        const url = new URL(image_url);
+
+        return url.hostname === 'images.unsplash.com';
+    } catch (error) {
+        return false;
+    }
+}, {
+    message: "Currently we are support only the image from unsplash",
+    path: ["image_url"],
+})
+
+export type BlogFormSchemaType = z.infer<typeof BlogFormSchema>;
