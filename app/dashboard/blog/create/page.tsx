4c1b8dd57d19001be196b504206749a207561bbd
albert19_ <albertnanda1902@gmail.com>
2023-12-17 11:22:26 +0800

    create insert blog data to supabase database

--- app/dashboard/blog/create/page.tsx
+++ app/dashboard/blog/create/page.tsx
@@ -4,18 +4,32 @@ import React from 'react'
 import BlogForm from '../../components/BlogForm'
 import { BlogFormSchemaType } from '../../schema'
 import { toast } from '@/components/ui/use-toast'
+import { createBlog } from '@/lib/actions/blog'
 
 const page = () => {
 
-    const handleCreate = (data: BlogFormSchemaType) => {
-        toast({
-            title: "You submitted the following values:",
-            description: (
-                <pre className="mt-2 w-full rounded-md bg-slate-950 p-4">
-                    <code className="text-white">{JSON.stringify(data, null, 2)}</code>
-                </pre>
-            ),
-        })
+    const handleCreate = async (data: BlogFormSchemaType) => {
+        const result = await createBlog(data)
+        const { error } = JSON.parse(result);
+
+        if (error?.message) {
+            toast({
+                title: "Fail to create blog",
+                description: (
+                    <pre className="mt-2 w-full rounded-md bg-slate-950 p-4">
+                        <code className="text-white">
+                            {error.message}
+                        </code>
+                    </pre>
+                ),
+            })
+        } else {
+            toast({
+                title: "Successfully created" + data.title,
+
+            })
+        }
+
     }
 
     return (
